040a1fdebde35c519cd941e90bc89db919e1e9c9
xnanax51 <xnanax51@uw.edu>
2022-11-14 15:38:43 -0800

    fixing codes

--- source/chart-2_code.R
+++ source/chart-2_code.R
@@ -3,8 +3,12 @@
 library("tidyverse")
 library("ggplot2")
 
+review_from_1000 <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-2-section-aa/main/data/Hotel%20Revires%20(1000%20hotels).csv")
+
+review_from_booking <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-2-section-aa/main/data/Hotels%20Reviews%20(booking.com).csv")
+
 new_data <- review_from_booking %>%
-  filter(Number.of.reviewers > 100) %>%
+  filter(Number.of.reviewers >= 0) %>%
   group_by(name)%>%
   summarise(number_of_reviewers = max(Number.of.reviewers, na.rm= TRUE)) %>%
   arrange(desc(number_of_reviewers)) %>%
