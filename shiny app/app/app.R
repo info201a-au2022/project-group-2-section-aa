55ef8053556918787b4671315a80f776d4ab74ee
xnanax51 <xnanax51@uw.edu>
2022-12-02 14:30:28 -0800

    nana

--- shiny app/app/app.R
+++ shiny app/app/app.R
@@ -9,6 +9,10 @@
 
 library(shiny)
 
+
+source("app_ui.R")
+source("app_server.R")
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -35,17 +39,12 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+    
 }
+
+
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
